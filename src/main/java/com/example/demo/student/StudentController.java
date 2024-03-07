a88960b8c83bc6a3f3186ef5ecbbe1f6086d4255
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2024-03-07 12:49:27 +0700

    Completed Features

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,33 +1,31 @@
 package com.example.demo.student;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import javax.validation.Valid;
 import java.util.List;
 
-@Slf4j
 @RestController
 @RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getAllStudents() {
-        List<Student> students = Arrays.asList(
-                new Student(
-                        1L,
-                        "Jamila",
-                        "jamila@amigoscode.edu",
-                        Gender.FEMALE),
-                new Student(
-                        2L,
-                        "Alex",
-                        "alex@amigoscode.edu",
-                        Gender.MALE)
-        );
-        log.info("students -> {}", students);
-        return students;
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addStudent(@Valid @RequestBody Student student) {
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+            @PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
